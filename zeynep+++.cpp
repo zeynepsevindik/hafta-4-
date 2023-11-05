fffadbd4eccb3626f9d91381b465d8b0d06f902f
zeynepsevindik <148634654+zeynepsevindik@users.noreply.github.com>
2023-11-05 15:25:12 +0300

    HAFTA 4 ÖDEVİ
    İNŞAALLAH DOĞRUDUR HOCAM

---
+++ zeynep+++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float yaricap,alan,cevre;
+    cout<<"Yaricapi Girin : ";
+    cin>>yaricap;
+    alan=3.14*yaricap*yaricap;
+    cevre=2*(3.14*yaricap);
+    cout<<"Alani : "<<alan<<endl;
+    cout<<"Cevresi : "<<cevre;
+}
